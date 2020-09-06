2a0144c8b6f5a5ae4b07660a3005658067e1c718
TheLordWXY <57043402+TheLordWXY@users.noreply.github.com>
2020-09-06 21:29:02 +0800

    建立springboot项目 连接数据库

---
+++ source_manager_server/src/main/java/com/example/demo/SourceManagerServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class SourceManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceManagerServerApplication.class, args);
+	}
+
+}
