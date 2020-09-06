158ce002778b536d81aa2959a96bddda6e70a15b
tjdxwwj <1090656420@qq.com>
2020-09-06 11:48:33 +0800

    主题页面完成

--- program/pages/cart/index.js
+++ program/pages/cart/index.js
@@ -1,54 +1,94 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    cateItems: [
+      {
+        cate_id: 1,
+        cate_name: "场地租借",
+        ishaveChild: true,
+        children:
+        [
+          {
+            child_id: 1,
+            name: '羽毛球',
+            image: "/images/badminton.jpg"
+          },
+          {
+            child_id: 2,
+            name: '篮球',
+            image: "/images/basketball.jpg"
+          },
+          {
+            child_id: 3,
+            name: '排球',
+            image:  "/images/volleyball.jpg"
+          },
+          {
+            child_id: 4,
+            name: '乒乓球',
+            image: "/images/Tabletennis.jpg"
+          },
+          {
+            child_id: 5,
+            name: '会议室',
+            image: "/images/meeting.jpg"
+          }
+        ]
+      },
+      {
+        cate_id: 2,
+        cate_name: "物品租借",
+        ishaveChild: true,
+        children:
+        [
+          {
+            child_id: 1,
+            name: '学习用品',
+            image: "/images/study.jpg"
+          },
+          {
+            child_id: 2,
+            name: '生活用品',
+            image: "/images/life.jpg"
+          },
+          {
+            child_id: 2,
+            name: '交通出行',
+            image: "/images/transport.jpg"
+          }
+        ]
+      },
+      {
+        cate_id: 3,
+        cate_name: "人力租借",
+        ishaveChild: true,
+        children:
+        [
+          {
+            child_id: 1,
+            name: '跑腿',
+            image: "/images/run.jpg"
+          },
+          {
+            child_id: 2,
+            name: '辅导',
+            image: "/images/help.jpg"
+          },
+        ]
+      },
+    ],
+    curNav: 1,
+    curIndex: 0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+  //事件处理函数  
+  switchRightTab: function (e) {
+    // 获取item项的id，和数组的下标值  
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    // 把点击到的某一项，设为当前index  
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      curNav: id,
+      curIndex: index
     })
   }
-})
+}) 
